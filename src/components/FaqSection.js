227104da842aa91bb7b8f7be809894db9b72b6c7
Himanshu chauhan <hc72701711@gmail.com>
2022-10-09 20:31:21 +0530

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start ?</h4>
+        <div className="answers">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, nulla.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answers">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, nulla.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4> Different Pyment Methods</h4>
+        <div className="answers">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, nulla.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Product do you offer.</h4>
+        <div className="answers">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, nulla.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,11 +2,13 @@ import React from "react";
 // Importing aboutsection
 import Aboutsection from "../components/AboutSection";
 import Servicesection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 const AboutUs = () => {
   return (
     <div>
       <Aboutsection />
       <Servicesection />
+      <FaqSection />
     </div>
   );
 };
